52663c32e51285ab4cffe7fae8cf6ab9453bd791
Fred Willmore <fredwillmore@gmail.com>
2017-03-25 00:04:19 +0100

    rewrite scale and precision in decimal fields

--- db/migrate/20170319150840_create_asteroids.rb
+++ db/migrate/20170319150840_create_asteroids.rb
@@ -2,27 +2,27 @@ class CreateAsteroids < ActiveRecord::Migration
   def change
     create_table :asteroids do |t|
       t.string :full_name
-      t.decimal :a, :precision => 16, :scale => 16
-      t.decimal :e, :precision => 16, :scale => 16
-      t.decimal :i, :precision => 16, :scale => 16
-      t.decimal :om, :precision => 16, :scale => 16
-      t.decimal :w, :precision => 16, :scale => 16
-      t.decimal :q, :precision => 16, :scale => 16
-      t.decimal :ad, :precision => 16, :scale => 16
-      t.decimal :per_y, :precision => 16, :scale => 16
+      t.decimal :a, precision: 16, scale: 8
+      t.decimal :e, precision: 16, scale: 8
+      t.decimal :i, precision: 16, scale: 8
+      t.decimal :om, precision: 16, scale: 8
+      t.decimal :w, precision: 16, scale: 8
+      t.decimal :q, precision: 16, scale: 8
+      t.decimal :ad, precision: 16, scale: 8
+      t.decimal :per_y, precision: 16, scale: 8
       t.integer :data_arc
       t.integer :condition_code
       t.integer :n_obs_used
       t.integer :n_del_obs_used
       t.integer :n_dop_obs_used
-      t.decimal :h, precision: 8, scale: 8
-      t.decimal :diameter, precision: 8, scale: 8
+      t.decimal :h, precision: 16, scale: 8
+      t.decimal :diameter, precision: 16, scale: 8
       t.string :extent
-      t.decimal :albedo, precision: 8, scale: 8
-      t.decimal :rot_per, precision: 8, scale: 8
-      t.decimal :gm, precision: 8, scale: 8
-      t.decimal :bv, precision: 8, scale: 8
-      t.decimal :ub, precision: 8, scale: 8
+      t.decimal :albedo, precision: 16, scale: 8
+      t.decimal :rot_per, precision: 16, scale: 8
+      t.decimal :gm, precision: 16, scale: 8
+      t.decimal :bv, precision: 16, scale: 8
+      t.decimal :ub, precision: 16, scale: 8
       t.string :spec_b
       t.string :spec_t
       t.timestamps null: false
--- db/migrate/20170319150852_create_comets.rb
+++ db/migrate/20170319150852_create_comets.rb
@@ -2,32 +2,32 @@ class CreateComets < ActiveRecord::Migration
   def change
     create_table :comets do |t|
       t.string :full_name
-      t.decimal :e, :precision => 16, :scale => 16
-      t.decimal :q, :precision => 16, :scale => 16
-      t.decimal :i, :precision => 16, :scale => 16
-      t.decimal :om, :precision => 16, :scale => 16
-      t.decimal :w, :precision => 16, :scale => 16
-      t.decimal :ad, :precision => 16, :scale => 16
-      t.decimal :tp_cal, :precision => 16, :scale => 16
-      t.decimal :per_y, :precision => 16, :scale => 16
+      t.decimal :e, :precision => 16, :scale => 8
+      t.decimal :q, :precision => 16, :scale => 8
+      t.decimal :i, :precision => 16, :scale => 8
+      t.decimal :om, :precision => 16, :scale => 8
+      t.decimal :w, :precision => 16, :scale => 8
+      t.decimal :ad, :precision => 16, :scale => 8
+      t.decimal :tp_cal, :precision => 16, :scale => 8
+      t.decimal :per_y, :precision => 16, :scale => 8
       t.string :comet_class
       t.integer :data_arc
       t.integer :condition_code
       t.integer :n_obs_used
       t.string :two_body
-      t.decimal :a1, :precision => 16, :scale => 16
-      t.decimal :a2, :precision => 16, :scale => 16
-      t.decimal :a3, :precision => 16, :scale => 16
-      t.decimal :dt, :precision => 16, :scale => 16
-      t.decimal :m1, precision: 8, scale: 8
+      t.decimal :a1, :precision => 16, :scale => 8
+      t.decimal :a2, :precision => 16, :scale => 8
+      t.decimal :a3, :precision => 16, :scale => 8
+      t.decimal :dt, :precision => 16, :scale => 8
+      t.decimal :m1, precision: 16, scale: 8
       t.integer :k1
-      t.decimal :m2, precision: 8, scale: 8
+      t.decimal :m2, precision: 16, scale: 8
       t.integer :k2
-      t.decimal :pc, precision: 8, scale: 8
-      t.decimal :diameter, precision: 8, scale: 8
+      t.decimal :pc, precision: 16, scale: 8
+      t.decimal :diameter, precision: 16, scale: 8
       t.string :extent
-      t.decimal :albedo, precision: 8, scale: 8
-      t.decimal :rot_per, precision: 8, scale: 8
+      t.decimal :albedo, precision: 16, scale: 8
+      t.decimal :rot_per, precision: 16, scale: 8
       t.timestamps null: false
     end
   end
