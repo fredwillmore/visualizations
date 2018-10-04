44581e579667b289fb72f6ccf42f0f7313cc2927
Fred Willmore <fredwillmore@gmail.com>
2018-10-03 21:31:30 -0700

    removed requires from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require d3
-//= require react
-//= require react_ujs
-//= require components
 //= require_tree .
